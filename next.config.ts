bbc13d29f66796d154d6b4887a6c7d6c4ce9a9be
lux2ube <lux2ube@gmail.com>
2025-07-06 18:36:05 +0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,29 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    watchOptions: {
+      ignored: ['**/.wwebjs_auth/**'],
+    },
+  },
+};
+
+export default nextConfig;
